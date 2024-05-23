bbf5bf542a1468ef7091757550559826cf618240
Divu <divyeshkuchhadia@gmail.com>
2024-05-23 21:45:12 +0530

    excersice 1.3.4 done

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -3,7 +3,12 @@ import Header from './components/Header';
 
 
 function App() {
-  return <Header firstName = "Virat Kohli"/>
+  return (
+    <>
+    <Header firstName="Virat Kohli" />
+    <Header firstName="Rohit Sharma Mumbai cha raja" />
+    </>
+  );
 }
 
 export default App;
