1c1a3e690406a8136f3968c7375ca2f08c0b66c4
Divu <divyeshkuchhadia@gmail.com>
2024-06-24 21:28:22 +0530

    excercise 3.2.4 completed

--- practiceapp/src/App.js
+++ practiceapp/src/App.js
@@ -1,22 +1,10 @@
 import React from "react";
-import Homepage from "./components/Homepage";
-import AboutMe from "./components/AboutMe";
-import {Route,Routes,Link} from 'react-router-dom'
-import Contact from "./components/Contact";
+import Image from "./Image";
 
 function App() {
     return(
         <div>
-            <nav>
-                <Link to="/" className="nav-item">Homepage</Link>
-                <Link to="/aboutMe" className="nav-item">AboutMe</Link>
-                <Link to="/contact" className="nav-item">Contact</Link>
-            </nav>
-            <Routes>
-                <Route path="/" element={<Homepage/>}/>
-                <Route path="/aboutMe" element={<AboutMe/>}/>
-                <Route path="/contact" element={<Contact/>}/>
-            </Routes>
+            <Image/>
         </div>
     );
 };
