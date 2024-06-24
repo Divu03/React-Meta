105082cf3eb61089fe525ceca5b8e2666c05b148
Divu <divyeshkuchhadia@gmail.com>
2024-06-24 21:51:46 +0530

    video asset in React 3.2.8

--- practiceapp/src/App.js
+++ practiceapp/src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Image from "./Image";
+import ReactPlayer from "react-player/youtube";
 
 function App() {
     return(
         <div>
             <Image/>
+            <Video/>
         </div>
     );
 };
+
+function Video(){
+    return (
+        <ReactPlayer url='https://www.youtube.com/watch?v=ee4mNBnU-rE' />
+      );
+}
 
 export default App;
