43da8c4dc29aad7f3f8e9423c443ec4add427f16
Divu <divyeshkuchhadia@gmail.com>
2024-06-24 16:53:33 +0530

    starting code for  excersice 3.1.4

--- practiceapp/src/App.js
+++ practiceapp/src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-
+import Homepage from "./components/Homepage";
+import AboutMe from "./components/AboutMe";
+import {Route,Routes,Link} from 'react-router-dom'
 
 function App() {
-
+    return(
+        <div>
+            <nav>
+                <Link to="/" className="nav-item">Homepage</Link>
+                <Link to="/aboutMe" className="nav-item">AboutMe</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Homepage/>}/>
+                <Route path="/aboutMe" element={<AboutMe/>}/>
+            </Routes>
+        </div>
+    );
 };
 
 export default App;
